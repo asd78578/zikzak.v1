b17aafac4fb1acfbb0f050fc9afef5928e138d32
tricky <alavutdin055@gmail.com>
2026-08-23 19:00:50 +0300

    feat(user): initialize user profile service

---
+++ user-service/src/test/java/com/example/zikzak/userservice/UserServiceApplicationTests.java
@@ -0,0 +1,12 @@
+package com.example.zikzak.userservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserServiceApplicationTests extends PostgresContainerTest {
+
+    @Test
+    void contextLoads() {
+    }
+}
