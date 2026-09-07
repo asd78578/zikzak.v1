d22a55b580ac6537c5ad43a76ebbfd7715f8042c
tricky <alavutdin055@gmail.com>
2026-09-07 22:21:31 +0300

    feat(chat): add Kafka retry and dead letter topic

---
+++ chat-service/src/main/java/com/example/zikzak/chatservice/config/KafkaConsumerConfig.java
@@ -0,0 +1,40 @@
+package com.example.zikzak.chatservice.config;
+
+import org.apache.kafka.common.TopicPartition;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.kafka.listener.DefaultErrorHandler;
+import org.springframework.kafka.listener.DeadLetterPublishingRecoverer;
+import org.springframework.util.backoff.FixedBackOff;
+
+@Configuration
+public class KafkaConsumerConfig {
+
+    @Bean
+    public DefaultErrorHandler kafkaErrorHandler(
+            KafkaTemplate<Object, Object> kafkaTemplate
+    ) {
+
+        DeadLetterPublishingRecoverer recoverer =
+                new DeadLetterPublishingRecoverer(
+                        kafkaTemplate,
+                        (record, exception) ->
+                                new TopicPartition(
+                                        record.topic() + ".DLT",
+                                        record.partition()
+                                )
+                );
+
+        FixedBackOff fixedBackOff =
+                new FixedBackOff(
+                        1000L,
+                        2L
+                );
+
+        return new DefaultErrorHandler(
+                recoverer,
+                fixedBackOff
+        );
+    }
+}
